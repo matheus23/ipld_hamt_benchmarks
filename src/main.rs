be5b45caa04c7be134c4cd46b1e07af42d471d01
Philipp Krüger <philipp.krueger1@gmail.com>
2022-09-02 11:11:02 +0200

    Use fvm hamt implementation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use forest_db::{Error, Store};
-use ipld_blockstore::BlockStore;
-use ipld_hamt::Hamt;
+use anyhow::Result;
+use cid::Cid;
+use fvm_ipld_blockstore::Blockstore;
+use fvm_ipld_hamt::Hamt;
 use parking_lot::RwLock;
-use std::collections::{hash_map::DefaultHasher, HashMap};
-use std::hash::{Hash, Hasher};
+use std::collections::HashMap;
 
 fn main() {
     ExperimentResult::print_csv_header();
@@ -81,19 +81,10 @@ fn experiment(bit_width: u32, n: usize, m: usize) -> ExperimentResult {
 /// A thread-safe `HashMap` wrapper.
 #[derive(Debug, Default)]
 pub struct MemoryDB {
-    db: RwLock<HashMap<u64, Vec<u8>>>,
+    db: RwLock<HashMap<Vec<u8>, Vec<u8>>>,
 }
 
 impl MemoryDB {
-    fn db_index<K>(key: K) -> u64
-    where
-        K: AsRef<[u8]>,
-    {
-        let mut hasher = DefaultHasher::new();
-        key.as_ref().hash::<DefaultHasher>(&mut hasher);
-        hasher.finish()
-    }
-
     fn bytes_stored(&self) -> u64 {
         let map = self.db.read().clone();
         let mut count: u64 = 0;
@@ -112,39 +103,17 @@ impl Clone for MemoryDB {
     }
 }
 
-impl Store for MemoryDB {
-    fn write<K, V>(&self, key: K, value: V) -> Result<(), Error>
-    where
-        K: AsRef<[u8]>,
-        V: AsRef<[u8]>,
-    {
-        self.db
-            .write()
-            .insert(Self::db_index(key), value.as_ref().to_vec());
+impl Blockstore for MemoryDB {
+    fn has(&self, k: &Cid) -> Result<bool> {
+        Ok(self.db.read().contains_key(&k.to_bytes()))
+    }
+
+    fn get(&self, k: &Cid) -> Result<Option<Vec<u8>>> {
+        Ok(self.db.read().get(&k.to_bytes()).cloned())
+    }
+
+    fn put_keyed(&self, k: &Cid, block: &[u8]) -> Result<()> {
+        self.db.write().insert(k.to_bytes(), block.into());
         Ok(())
-    }
-
-    fn delete<K>(&self, key: K) -> Result<(), Error>
-    where
-        K: AsRef<[u8]>,
-    {
-        self.db.write().remove(&Self::db_index(key));
-        Ok(())
-    }
-
-    fn read<K>(&self, key: K) -> Result<Option<Vec<u8>>, Error>
-    where
-        K: AsRef<[u8]>,
-    {
-        Ok(self.db.read().get(&Self::db_index(key)).cloned())
-    }
-
-    fn exists<K>(&self, key: K) -> Result<bool, Error>
-    where
-        K: AsRef<[u8]>,
-    {
-        Ok(self.db.read().contains_key(&Self::db_index(key)))
     }
 }
-
-impl BlockStore for MemoryDB {}
